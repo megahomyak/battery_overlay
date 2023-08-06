1fd9a006a542d05bc5e8f043e6170677fc2256a1
megahomyak <g.megahomyak@gmail.com>
2023-08-07 01:48:51 +0500

    Made the MVP with random assets

--- battery_overlay.py
+++ battery_overlay.py
@@ -1,5 +1,3 @@
-import time
-
 from PyQt5.QtCore import QPoint, Qt, QTimer
 from PyQt5.QtGui import QPainter, QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -7,15 +5,30 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import psutil
 
 
+ACTIVATION_PERCENTAGES = list(range(100))
+
+
 class BatteryIndicator(QMainWindow):
+    def check_battery_level(self):
+        percentage = int(psutil.sensors_battery().percent)
+        print(percentage)
+        if percentage < self.last_percentage and percentage in ACTIVATION_PERCENTAGES:
+            print("in")
+            self.show()
+            QTimer.singleShot(5000, self.hide)
+        self.last_percentage = percentage
 
     def __init__(self):
         super().__init__()
 
+        self.last_percentage = int(psutil.sensors_battery().percent)
+
         self.setWindowOpacity(0.5)
         self.lastPoint = QPoint()
         self.background = QPixmap("../../website_icon.png")
-        QTimer.singleShot(5000, QApplication.quit)
+        self.check_timer = QTimer()
+        self.check_timer.timeout.connect(self.check_battery_level)
+        self.check_timer.start(500)
         width = self.background.width()
         height = self.background.height()
         x = 300
@@ -25,6 +38,8 @@ class BatteryIndicator(QMainWindow):
             self.windowFlags()
             | Qt.WindowTransparentForInput
             | Qt.X11BypassWindowManagerHint
+            | Qt.FramelessWindowHint
+            | Qt.WindowStaysOnTopHint
         )
 
     def paintEvent(self, _event):
@@ -34,5 +49,4 @@ class BatteryIndicator(QMainWindow):
 
 app = QApplication([])
 window = BatteryIndicator()
-window.show()
 app.exec()
