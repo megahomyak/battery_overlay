dc6e66625c1438e38f3d7e6dea64d3c550f64dbb
megahomyak <g.megahomyak@gmail.com>
2023-08-11 03:13:29 +0500

    FUCK OPACITY I'M OUT

--- battery_overlay.py
+++ battery_overlay.py
@@ -15,47 +15,11 @@ PERCENTAGES_TO_IMAGES = {
 
 
 class BatteryIndicator(QMainWindow):
-    def hide_window(self):
-        effect = QGraphicsOpacityEffect(self)
-        self.anim = QPropertyAnimation(effect, b"opacity")
-        self.anim.setStartValue(1)
-        self.anim.setDuration(20000)
-        self.anim.setEasingCurve(QEasingCurve.Type.Linear)
-        self.anim.setEndValue(0)
-        self.anim.start(QAbstractAnimation.DeletionPolicy.DeleteWhenStopped)
-
-    def show_battery_level(self, percentage):
-        image = PERCENTAGES_TO_IMAGES[percentage]
-        screen_size = app.primaryScreen().size()
-        pixel_ratio = app.devicePixelRatio() ** -1
-        width = int(image.width * pixel_ratio)
-        height = int(image.height * pixel_ratio)
-        x = screen_size.width() - width
-        y = screen_size.height() - height
-        self.setGeometry(x, y, width, height)
-
-        effect = QGraphicsOpacityEffect(self)
-        self.anim = QPropertyAnimation(effect, b"opacity")
-        self.anim.setStartValue(0)
-        self.anim.setEasingCurve(QEasingCurve.Type.Linear)
-        self.anim.setDuration(20000)
-        self.anim.setEndValue(1)
-        self.anim.start(QAbstractAnimation.DeletionPolicy.DeleteWhenStopped)
-
-        self.background = QPixmap.fromImage(ImageQt.ImageQt(image))
-        QTimer.singleShot(25000, self.hide_window)
-
-    def check_battery_level(self):
-        percentage = int(psutil.sensors_battery().percent)
-        if (
-                percentage < self.last_percentage
-                and percentage in PERCENTAGES_TO_IMAGES
-        ):
-            self.show_battery_level(percentage)
-        self.last_percentage = percentage
 
     def __init__(self):
         super().__init__()
+
+        self.painter = QPainter(self)
 
         self.last_percentage = int(psutil.sensors_battery().percent)
 
@@ -75,9 +39,48 @@ class BatteryIndicator(QMainWindow):
         self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
         self.setAttribute(Qt.WidgetAttribute.WA_TransparentForMouseEvents)
 
+    def hide_window(self):
+        effect = QGraphicsOpacityEffect(self)
+        self.anim = QPropertyAnimation(effect, b"opacity")
+        self.anim.setStartValue(1)
+        self.anim.setDuration(20000)
+        self.anim.setEasingCurve(QEasingCurve.Type.Linear)
+        self.anim.setEndValue(0)
+        self.anim.start(QAbstractAnimation.DeletionPolicy.DeleteWhenStopped)
+
+    def show_battery_level(self, percentage):
+        image = PERCENTAGES_TO_IMAGES[percentage]
+        screen_size = app.primaryScreen().size()
+        pixel_ratio = app.devicePixelRatio() ** -1
+        width = int(image.width * pixel_ratio)
+        height = int(image.height * pixel_ratio)
+        x = screen_size.width() - width
+        y = screen_size.height() - height
+        self.setGeometry(x, y, width, height)
+
+        self.anim = QPropertyAnimation(self.painter, b"opacity")
+        self.anim.setStartValue(0)
+        self.anim.setEasingCurve(QEasingCurve.Type.Linear)
+        self.anim.setDuration(20000)
+        self.anim.setEndValue(1)
+        self.anim.start(QAbstractAnimation.DeletionPolicy.DeleteWhenStopped)
+
+        self.background = QPixmap.fromImage(ImageQt.ImageQt(image))
+        QTimer.singleShot(25000, self.hide_window)
+
+    def check_battery_level(self):
+        percentage = int(psutil.sensors_battery().percent)
+        if (
+                percentage < self.last_percentage
+                and percentage in PERCENTAGES_TO_IMAGES
+        ):
+            self.show_battery_level(percentage)
+        self.last_percentage = percentage
+
     def paintEvent(self, _event):
-        painter = QPainter(self)
-        painter.drawPixmap(self.rect(), self.background)
+        self.painter.begin(self)
+        self.painter.drawPixmap(self.rect(), self.background)
+        self.painter.end()
 
 app = QApplication([])
 window = BatteryIndicator()
