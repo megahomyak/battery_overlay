828ea567e247a4378c1c8b214b8213b8b74ce4af
megahomyak <g.megahomyak@gmail.com>
2023-08-23 21:11:35 +0500

    Tried to mess with Qt, but FUCK IT

--- battery_overlay.cpp
+++ battery_overlay.cpp
@@ -1,36 +1,40 @@
 #include <QApplication>
 #include <QLabel>
+#include <Qt>
 #include <QtGui>
 
 #include <stdio.h>
 #include <stdlib.h>
 
 void die(const char *text) {
-  perror(text);
-  exit(-1);
+    perror(text);
+    exit(-1);
 }
 
 #ifdef __linux__
 int get_percentage() {
-  int percentage;
-  FILE *batteryFile = fopen("/sys/class/power_supply/BAT1/capacity", "r");
-  if (batteryFile == NULL) {
-    die("no battery file found");
-  }
-  if (fscanf(batteryFile, "%d", &percentage) != 1) {
-    die("can't read the percentage from the battery file");
-  }
-  fclose(batteryFile);
-  return percentage;
+    int percentage;
+    FILE *batteryFile = fopen("/sys/class/power_supply/BAT1/capacity", "r");
+    if (batteryFile == NULL) {
+        die("no battery file found");
+    }
+    if (fscanf(batteryFile, "%d", &percentage) != 1) {
+        die("can't read the percentage from the battery file");
+    }
+    fclose(batteryFile);
+    return percentage;
 }
 #endif
 
 int main(int argc, char **argv) {
-  QApplication app(argc, argv);
-  QPixmap pixmap;
-  printf("%d", pixmap.load(":/battery.png"));
-  QLabel label;
-  label.setPixmap(pixmap);
-  label.show();
-  return app.exec();
+    QApplication app(argc, argv);
+    QPixmap pixmap;
+    printf("%d", pixmap.load(":/battery.png"));
+    QLabel label(
+        nullptr,
+        Qt::WindowType::FramelessWindowHint
+    );
+    label.setPixmap(pixmap);
+    label.show();
+    return app.exec();
 }
