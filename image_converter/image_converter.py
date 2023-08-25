71b8c34e950029f552d9e59a24842f22ec9dbb88
megahomyak <g.megahomyak@gmail.com>
2023-08-25 13:35:58 +0500

    Fuuuuuuck that I'm doing it THE EASIER WAY

--- image_converter/image_converter.py
+++ image_converter/image_converter.py
@@ -4,19 +4,56 @@ import argparse
 import json
 from dataclasses import dataclass
 
+# include (for the funny ZPixmap)
+# image1
+# image2
+# image3
+# percentage: image pointer
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--output")
 parser.add_argument("--input")
-
 args = parser.parse_args()
 
-with open(args.input, encoding="utf-8") as f:
-    listing = json.loads(f.read())
+
+@dataclass
+class ImageData:
+    array_definition: str
+    width: int
+    height: int
+
+def sort_by_percentage(listing):
+    percentage_to_image_path = {}
+
+    for entry in listing:
+        path = entry["image_path"]
+        percentage = entry["battery_percentage"]
+        if percentage in percentage_to_image_path:
+            raise Exception(f"Percentage {percentage} occurs twice")
+        percentage_to_image_path[percentage] = path
+
+    return percentage_to_image_path
+
+def generate_by_image(percentage_to_image_path):
+    percentage_to_image_data = {}
+    last_image_number = 0
+    for percentage, image_path in percentage_to_image_path:
+        array_name = f"IMG{last_image_number}"
+        last_image_number += 1
+        image_data = generate(image_path, array_name)
+        percentage_to_image_data[percentage] = image_data
+    return percentage_to_image_data
+
+def main():
+    # This is done to prevent variable leak
+    with open(args.input, encoding="utf-8") as f:
+        listing = json.loads(f.read())
+    percentage_to_image_path = sort_by_percentage(listing)
+    percentage_to_image_data = generate_by_image(percentage_to_image_path)
 
 def generate(image_path, array_name):
     array_definition = f"unsigned char {array_name}[] = {{\n"
-    file_path = os.path.join(listing_directory, image_data["file_path"])
-    image = Image.open(file_path)
+    image = Image.open(image_path)
     for pixel in image.getdata():
         red, green, blue, opacity = pixel
         for color_value in [red, green, blue, opacity]:
@@ -24,7 +61,11 @@ def generate(image_path, array_name):
             array_definition += ","
         array_definition += "\n"
     array_definition += "};"
-    return 
+    return ImageData(
+        array_definition=array_definition,
+        width=image.width,
+        height=image.height,
+    )
 
 X11_INCLUSION = "#include <X11/Xlib.h>"
 
@@ -111,3 +152,5 @@ output.append(image_array_definition)
 
 with open(args.output, "w", encoding="utf-8") as f:
     f.write("\n\n".join(output))
+
+main()
